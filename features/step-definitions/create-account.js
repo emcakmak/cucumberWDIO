9e0ef2aaf218b24441041b243f8befdd769b2d81
Emre Cakmak <emrecakmak93@hotmail.co.uk>
2022-06-20 22:15:52 +0100

    Add create-account.js file

---
+++ features/step-definitions/create-account.js
@@ -0,0 +1,20 @@
+const { Given, When, Then } = require('@wdio/cucumber-framework')
+
+Given("I am on the signin page", async () => {
+
+})
+And("I enter email <email>", async () => {
+
+})
+And("I click the create account button", async () => {
+
+})
+When("I enter my <name> <lastname> <email> and <password>", async () => {
+
+})
+And("I click the register button", async () => {
+
+})
+Then("I am taken to My Account page", async () => {
+
+})
